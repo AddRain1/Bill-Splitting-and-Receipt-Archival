a715c1323e2d4186f805437fcf9811e690caa8a5
Angie-n <68796619+Angie-n@users.noreply.github.com>
2024-07-11 10:54:04 -0700

    authentication

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const session = require('express-session');
+const passport = require('passport');
+
+const SQLiteStore = require('connect-sqlite3')(session);
+
+const userRouter = require('./routes/auth');
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, (error) =>{
+    if(!error) console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+    else console.log("Error occurred, server can't start", error);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'password',
+  resave: false,
+  saveUninitialized: false,
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
+app.use(passport.authenticate('session'));
+
+app.use('/auth', userRouter);
