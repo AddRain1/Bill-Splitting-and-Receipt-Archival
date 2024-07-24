3ebf98ba4a1f22f1e3f0090eef6843944633d735
Angie-n <68796619+Angie-n@users.noreply.github.com>
2024-07-24 12:03:45 -0700

    Resolve merge conflicts

---
+++ backend/routes/auth.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oidc');
+const dotenv = require('dotenv');
+dotenv.config();
+//var db = require('../db');
+
+passport.use(new GoogleStrategy({
+    clientID: process.env['GOOGLE_CLIENT_ID'],
+    clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+    callbackURL: '/oauth2/redirect/google',
+    scope: [ 'profile' ]
+  }, function verify(issuer, profile, cb) {
+        /* db logic */
+      }
+));  
+
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+        cb(null, { id: user.id, username: user.username, name: user.name });
+    });
+});
+
+passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+        return cb(null, user);
+    });
+});
+
+router.get('/google',
+    passport.authenticate('google', { scope:[ 'email', 'profile' ] }
+));
+
+router.get( '/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/google/success',
+        failureRedirect: '/google/failure'
+}));
+
+router.get('/login/federated/google', passport.authenticate('google'));
+
+router.get('/oauth2/redirect/google', passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
+
+router.post('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+});
+
+module.exports = router;
