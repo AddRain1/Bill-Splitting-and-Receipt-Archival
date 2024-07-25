c0c33519c93ec4b6515aae817e11b2f8a29fd831
Daniel <daniel.luoj@gmail.com>
2024-07-25 16:58:00 -0700

    Cleaned up code, took out email log in part (not ready yet)

--- username_password/server.js
+++ username_password/server.js
@@ -18,34 +18,29 @@ initPassport(passport, async username => {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE
     });
-    const usernameQuery = 'SELECT * FROM users WHERE username = ?';
-    
-    const usernameField = [username];
-    const [results] = await connection.execute(usernameQuery, usernameField);
-    console.log('results.length is : '+ results.length)
-    if (results.length === 0){
+    if(username != null){
+        const usernameQuery = 'SELECT * FROM users WHERE username = ?';
+        const usernameField = [username];
+        var [userResults] = await connection.execute(usernameQuery, usernameField);
+
+        // const emailQuery = 'SELECT * FROM users WHERE email = ?';
+        // const emailField = [login];
+        // var [emailResults] = await connection.execute(emailQuery, emailField);
+        // console.log('emailresults.length is : '+ emailResults.length)
+    }
+
+
+    if (userResults.length != 0){
+        var ret = userResults.map(result => new User(result.user_id, result.username, result.email, result.firstname, result.lastname, result.hashed_password, result.profile_description));
+    }
+    // else if(emailResults.length != 0){
+    //     var ret = emailResults.map(result => new User(result.user_id, result.username, result.email, result.firstname, result.lastname, result.hashed_password, result.profile_description));
+    // }
+    else{
         return null;
     }
-    const ret = results.map(result => new User(result.user_id, result.username, result.email, result.firstname, result.lastname, result.hashed_password, result.profile_description));
     return ret[0];
 
-},  async email => {
-    const connection = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
-    });
-    const emailQuery = 'SELECT * FROM users WHERE email = ?';
-    
-    const emailField = [email];
-    const [results] = await connection.execute(emailQuery, emailField);
-    if (results.length === 0){
-        return null;
-    }
-    const ret = results.map(result => new User(result.user_id, result.username, result.email, result.firstname, result.lastname, result.hashed_password, result.profile_description));
-    
-    return ret[0];
 }, async id => {
     const connection = await mysql.createConnection({
         host: process.env.DB_HOST,
@@ -57,7 +52,6 @@ initPassport(passport, async username => {
     
     const idField = [id];
     const [results] = await connection.execute(idQuery, idField);
-    console.log('results.length is : '+ results.length)
     if (results.length === 0){
         return null;
     }
@@ -82,28 +76,22 @@ app.use(passport.session())
 
 app.use(methodOverride("_method"));
 
-
-
 app.get("/", checkAuthenticated, (req, res) => {
     res.render("index.ejs", {name: req.user.firstname});
 });
-
 
 app.get("/login", checkNotAuthenticated, (req, res) => {
     res.render("login.ejs");
 });
 
-
 app.get("/signup", checkNotAuthenticated, (req, res) => {
     res.render("signup.ejs");
 });
-
 
 app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login"
 }));
-
 
 app.post("/signup", checkNotAuthenticated, async (req, res) => {
     try{
@@ -148,4 +136,3 @@ function checkNotAuthenticated(req, res, next) {
 }
 
 app.listen(3000); 
-
--- username_password/user.js
+++ username_password/user.js
@@ -1,4 +1,3 @@
-
 export class User {
     user_id;
     username;
