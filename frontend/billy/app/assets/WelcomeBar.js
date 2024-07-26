fc7dac5f0f5071d76f6d3ee96c0ae820b6f10049
Angie-n <68796619+Angie-n@users.noreply.github.com>
2024-07-26 11:44:49 -0700

    biomejs check

--- frontend/billy/app/assets/WelcomeBar.js
+++ frontend/billy/app/assets/WelcomeBar.js
@@ -1,49 +1,48 @@
-import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView,Image } from 'react-native';
-import { useFonts } from 'expo-font';
+import { useFonts } from "expo-font";
+import React from "react";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 const WelcomeBar = ({ username }) => {
+	const [font] = useFonts({
+		SplineSansMono: require("/Users/arpitapandey/bill-splitting-and-receipt-archival-1/bill-splitting-and-receipt-archival/app/assets/fonts/SplineSansMono-Regular.ttf"),
+		PlayfairDisplay_400Regular: require("/Users/arpitapandey/bill-splitting-and-receipt-archival-1/bill-splitting-and-receipt-archival/app/assets/fonts/PlayfairDisplay-Regular.ttf"),
+		PlayfairDisplay_700Bold: require("/Users/arpitapandey/bill-splitting-and-receipt-archival-1/bill-splitting-and-receipt-archival/app/assets/fonts/PlayfairDisplay-Bold.ttf"),
+	});
 
-    const [font] = useFonts({
-        'SplineSansMono': require('/Users/arpitapandey/bill-splitting-and-receipt-archival-1/bill-splitting-and-receipt-archival/app/assets/fonts/SplineSansMono-Regular.ttf'),
-        'PlayfairDisplay_400Regular': require('/Users/arpitapandey/bill-splitting-and-receipt-archival-1/bill-splitting-and-receipt-archival/app/assets/fonts/PlayfairDisplay-Regular.ttf'),
-        'PlayfairDisplay_700Bold': require('/Users/arpitapandey/bill-splitting-and-receipt-archival-1/bill-splitting-and-receipt-archival/app/assets/fonts/PlayfairDisplay-Bold.ttf'),
-    });
-
-    if (!font) {
-     return <AppLoading />;
-    }
-  return (
-    <View style={styles.container}>
-      <Image source={require('/Users/arpitapandey/bill-splitting-and-receipt-archival-1/bill-splitting-and-receipt-archival/app/assets/logo.jpeg')} style ={styles.image}/>
-      <Text style={styles.welcomeText}>hello,  {username}</Text>
-    </View>
-  );
+	if (!font) {
+		return <AppLoading />;
+	}
+	return (
+		<View style={styles.container}>
+			<Image
+				source={require("/Users/arpitapandey/bill-splitting-and-receipt-archival-1/bill-splitting-and-receipt-archival/app/assets/logo.jpeg")}
+				style={styles.image}
+			/>
+			<Text style={styles.welcomeText}>hello, {username}</Text>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#ECF1F5',
-    flexDirection: 'row',
-    width: '100%',
-    height: 131,
-    width:430,
-    alignItems: 'center'
-    
-  },
-  welcomeText: {
-    color: '#000000',
-    fontSize: 22,
-    fontWeight: 'bold',
-    left: 50,
-    fontFamily: 'PlayfairDisplay_400Regular',
-  },
-  image: {
-    width:50,
-    height:50,
-    left:25,
-    borderRadius: 12,
-
-  }
+	container: {
+		backgroundColor: "#ECF1F5",
+		flexDirection: "row",
+		height: 131,
+		width: 430,
+		alignItems: "center",
+	},
+	welcomeText: {
+		color: "#000000",
+		fontSize: 22,
+		fontWeight: "bold",
+		left: 50,
+		fontFamily: "PlayfairDisplay_400Regular",
+	},
+	image: {
+		width: 50,
+		height: 50,
+		left: 25,
+		borderRadius: 12,
+	},
 });
 
-export default WelcomeBar; 
+export default WelcomeBar;
