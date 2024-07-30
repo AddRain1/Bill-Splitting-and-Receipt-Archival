b37b67ff5133bcb3a3f5beaab632326b4fa4d44e
Adrian Vo <akvo@ucsd.edu>
2024-07-30 16:55:54 -0700

    basic image conversion

---
+++ image-to-text/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import Tesseract from 'tesseract.js';
+import './App.css';
+// import preprocessImage from './preprocess';
+
+function App() {
+  const [image, setImage] = useState(null);
+  const [text, setText] = useState(null);
+
+  const handleChange = (event) => {
+    setImage(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const handleClick = () => {
+    if (!image) return;
+
+    Tesseract.recognize(
+      image, 'eng',
+      { 
+        logger: m => console.log(m) 
+      }
+    )
+    .then(result => {
+      setText(result.text);
+    })
+    .catch(err => {
+      console.error(err);
+      setText("An error occurred while processing the image.");
+    })
+  }
+
+  return (
+    <div className="App">
+      <main className="App-main">
+        <h3>Actual image uploaded</h3>
+        {image && <img src={image} className="App-image" alt="uploaded" />}
+        
+        <h3>Extracted text</h3>
+        <div className="text-box">
+        </div>
+        <input type="file" onChange={handleChange} />
+        <button onClick={handleClick} style={{ height: 50 }}>Convert to Text</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
