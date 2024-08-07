20d5d535dafd7c18f91405f32d89405f87815c7f
Angie-n <68796619+Angie-n@users.noreply.github.com>
2024-08-07 15:23:40 -0700

    receipt

--- backend/routes/receipt.js
+++ backend/routes/receipt.js
@@ -1,64 +1,134 @@
 const express = require('express');
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
-const Receipts = require("../class/receiptsClass.js");
+
+const Receipt = require("../class/receiptsClass.js");
 const receiptAPI = require("../api/receiptsAPI.js");
-const Tax = require("../class/taxClass.js");
-const Tip = require("../class/tipClass.js");
-const ExpenseRate = require("../class/expenseRateClass.js");
-const helper = require("../helpers/helper.js");
+const accessHelper = require('../helpers/access.js');
 
 //get a list of receipts from all of the user's groups
 //Authorization: Can only see receipts that are part of the user's groups.
 router.get('/', async (req, res) => {
-    const allReceiptArray = await receiptAPI.getAllReceipts();
-    res.send(allReceiptArray);    
+    const receipts = accessHelper.get_accessible_receipts(req.user);
+    res.sendStatus(200).json(JSON.stringify(receipts));
 });
 
 //create a new receipt
 //Authorization: Must be a member of the group that the receipt will be added to. 
-router.get('/add', async (req, res) => {
-    // get current time
-    const currentTime = new Date();
-    // generate receipt_id with current time
-    const rec_id = helper._date_to_string(currentTime);
-    const tax_nm = 'tax 4';
-    const tax_per = 2.0;
-    // tax information
-    const temp_tax = new Tax(0, rec_id, tax_nm, tax_per);
-    // tips information
-    const temp_tip = new Tip(0, rec_id, 44.44);
-    // expense_rate information
-    const temp_expense_rate = new ExpenseRate(0, rec_id, 'tempName', 10.00);
-    const options = {tax: temp_tax, tip: temp_tip, expense_rate: temp_expense_rate};
-    // receipt information
-    const temp_receipt = new Receipts(rec_id, 2, 'image3.jpg', 'receipt 3', 'description of receipt 3', 'food', currentTime, 'vendor 3', options);
-    // will add receipt to database and add tax/tips to database
-    await receiptAPI.addReceipt(temp_receipt);
-    const allReceiptArray = await receiptAPI.getAllReceipts();
-    console.log(allReceiptArray);
-    res.send(allReceiptArray);
-});
+router.get('/add', [
+    //TODO: Tax, tip, expense_rate, item handling
+    //TODO: Custom validation for images
+    body("images")
+        .trim()
+        .escape(),
+    body("name", "name must be 100 characters max")
+        .trim()
+        .isLength({max: 100})
+        .escape(),
+    body("description", "description must be 250 characters max")
+        .trim()
+        .isLength({max: 250})
+        .escape(),
+    //TODO: Comfirm validation for category
+    body("category", "category must be 100 characters max")
+        .trim()
+        .isLength({max: 100})
+        .escape(),
+    body("vendor", "vendor must be 250 characters max")
+        .trim()
+        .isLength({max: 250})
+        .escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if(!accessHelper.check_group_accessible(req.user, req.body.group_id)) {
+            res.sendStatus(401).json({msg: 'User must be a member of the group they link'});
+        }
+
+        const receipt = new Receipt({
+            group_id: req.body.group_id,
+            images: req.body.images,
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            vendor: req.body.vendor
+        });
+
+        if (errors.isEmpty()) {
+            receiptAPI.addReceipt(receipt);
+            
+            res.sendStatus(200).json(JSON.stringify(receipt));
+        }
+    }
+]);
 
 //get information of receipt with ID
 //Authorization: Must be a member of the group that the receipt is part of.
 router.get('/:id', async (req, res) => {
-
+    const receipt = receiptAPI.getReceiptByID(req.params.id);
+    if(!accessHelper.check_group_accessible(req.user, req.body.group_id)) {
+        res.sendStatus(401).json({msg: 'User must be a member of the group they link'});
+    }
+    else res.sendStatus(200).json(JSON.stringify(receipt));
 });
 
 //update receipt with ID
 //Authorization: Must be the admin of the receipt
-router.get('/:id/update', async (req, res) => {
+router.get('/:id/update', [
+    //TODO: Tax, tip, expense_rate, item handling
+    //TODO: Custom validation for images
+    body("images")
+        .trim()
+        .escape(),
+    body("name", "name must be 100 characters max")
+        .trim()
+        .isLength({max: 100})
+        .escape(),
+    body("description", "description must be 250 characters max")
+        .trim()
+        .isLength({max: 250})
+        .escape(),
+    //TODO: Comfirm validation for category
+    body("category", "category must be 100 characters max")
+        .trim()
+        .isLength({max: 100})
+        .escape(),
+    body("vendor", "vendor must be 250 characters max")
+        .trim()
+        .isLength({max: 250})
+        .escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
 
-});
+        const receipt = new Receipt({
+            images: req.body.images,
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            vendor: req.body.vendor
+        });
+
+        if (errors.isEmpty()) {
+            if(req.body.images) receiptAPI.changeReceipt(req.params.id, "images", req.body.images);
+            if(req.body.name) receiptAPI.changeReceipt(req.params.id, "name", req.body.name);
+            if(req.body.description) receiptAPI.changeReceipt(req.params.id, "description", req.body.description);
+            if(req.body.category) receiptAPI.changeReceipt(req.params.id, "category", req.body.category);
+            if(req.body.vendor) receiptAPI.changeReceipt(req.params.id, "name", req.body.vendor);
+            
+            res.sendStatus(200).json(JSON.stringify(receipt));
+        }
+    }
+]);
 
 //delete receipt with ID
 //Authorization: Must be the admin of the receipt
 router.get('/:id/delete', async (req, res) => {
-    // will not only delete receipt but also delete tax and tips
-    await receiptAPI.deleteReceipt('20230816000000');
-    const allReceiptArray = await receiptAPI.getAllReceipts();
-    console.log(allReceiptArray);
-    res.send(allReceiptArray);
+    const receipt = receiptAPI.getReceiptByID(req.params.id);
+    if(receipt.admin_id != req.user) res.sendStatus(401).json({msg: 'User must be an admin to delete the receipt'});
+    else {
+        receiptAPI.deleteReceipt(req.params.id);
+        res.sendStatus(200).json(JSON.stringify(receipt));
+    }
 });
 
 module.exports = router;
