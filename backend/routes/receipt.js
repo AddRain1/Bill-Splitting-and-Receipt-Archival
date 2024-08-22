4cd423e265c11c676fd6f174f531eb92f06e3124
Arpita Pandey <arpitapandeyyy@gmail.com>
2024-08-22 12:19:50 -0700

    removing duplicates

--- backend/routes/receipt.js
+++ backend/routes/receipt.js
@@ -62,7 +62,7 @@ router.post('/add', [
 
         if (errors.isEmpty()) {
             await receiptAPI.addReceipt(receipt);
-            await receiptAPI.addReceipt(receipt);
+           
             
             if(!res.headersSent) res.status(200).json(receipt);
         }
@@ -73,7 +73,7 @@ router.post('/add', [
 //Authorization: Must be a member of the group that the receipt is part of.
 router.get('/:id', async (req, res) => {
     const receipt = await receiptAPI.getReceiptByID(req.params.id);
-    const receipt = await receiptAPI.getReceiptByID(req.params.id);
+    
     if(!accessHelper.check_group_accessible(req.user, req.body.group_id)) {
         res.status(401).json({msg: 'User must be a member of the group they link'});
     }
