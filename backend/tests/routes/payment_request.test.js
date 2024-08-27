152be2db2acc2ffa705501c6495c5027ca5f108c
Angie-n <68796619+Angie-n@users.noreply.github.com>
2024-08-27 12:09:46 -0700

    Add invalid request tests for payment request

--- backend/tests/routes/payment_request.test.js
+++ backend/tests/routes/payment_request.test.js
@@ -145,8 +145,7 @@ describe("Payment request routes", () => {
           .expect("Content-Type", /json/)
           .expect(200)
           .then((response) => {      
-            const body = response.body;   
-            console.log(body);      
+            const body = response.body;    
             expect(body.length).toBe(0);
           })
           .catch((err) => {
@@ -213,7 +212,7 @@ describe("Payment request routes", () => {
           receiver_id: user_ids[1],
           amount: 250,
           description: "couch",
-          receipt_id: receipt_ids[0]
+          receipt_id: receipt_ids[1]
       }
       await request
           .post("/paymentrequests/add")
@@ -228,6 +227,27 @@ describe("Payment request routes", () => {
           .catch((err) => {
               expect(err).toBe(null);
           });
+    });
+
+    it("POST /paymentrequests/add - Attempt to create payment request when one user does not have access to the linked receipt", async () => {
+      const users = await userAPI.getUsers();
+      const user_ids = users.map(u => u.user_id);
+
+      const receipts = await receiptAPI.getReceipts();
+      const receipt_ids = receipts.map(r => r.receipt_id);
+      const payload = {
+          payer_id: user_ids[0], 
+          receiver_id: user_ids[1],
+          amount: 250,
+          description: "couch",
+          receipt_id: receipt_ids[0]
+      }
+      await request
+          .post("/paymentrequests/add")
+          .send(payload)
+          .set('Content-Type', 'application/json')
+          .set('Accept', 'application/json')
+          .expect(401)
     });
 
     it("POST /paymentrequests/add - Attempt to create a payment request that does not involve signed in user", async () => {
@@ -247,7 +267,6 @@ describe("Payment request routes", () => {
           .set('Accept', 'application/json')
           .expect(400)
     });
-});
 
 it("POST /paymentrequests/update - As the payer, decline payment request" , async () => {
   const payment_requests = await paymentRequestAPI.getPaymentRequests();
@@ -294,21 +313,90 @@ it("POST /paymentrequests/update - As the receiver, update information" , async 
       });
 });
 
-it("POST /paymentrequests/delete - As the receiver, delete the payment request before it is completed" , async () => {
-  const payment_requests = await paymentRequestAPI.getPaymentRequests();
-  const payment_request_ids = payment_requests.map(p => p.payment_request_id);
-  const initialLength = payment_requests.length;
-  
-  await request
-      .post("/paymentrequests/" + payment_request_ids[1] + "/delete")
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
+  it("POST /paymentrequests/delete - As the receiver, delete the payment request before it is completed" , async () => {
+    const payment_requests = await paymentRequestAPI.getPaymentRequests();
+    const payment_request_ids = payment_requests.map(p => p.payment_request_id);
+    const initialLength = payment_requests.length;
+    
+    await request
+        .post("/paymentrequests/" + payment_request_ids[1] + "/delete")
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(async() => {
+          const payment_requests = await paymentRequestAPI.getPaymentRequests();
+            expect(payment_requests.length).toBe(initialLength - 1);
+        })
+        .catch((err) => {
+            expect(err).toBe(null);
+        });
+  });
+
+  it("POST /paymentrequests/delete - As the payer, attempt to delete the payment request before it is completed" , async () => {
+    const payment_requests = await paymentRequestAPI.getPaymentRequests();
+    const payment_request_ids = payment_requests.map(p => p.payment_request_id);
+    
+    await request
+        .post("/paymentrequests/" + payment_request_ids[1] + "/delete")
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(401)
+  });
+
+  it("Logout and login as another user", async () => {
+    //Login as the third created user
+    const payload = {
+        username: user3_payload.username,
+        password: user3_payload.password
+    }
+    await request
+      .post("/auth/logout")
       .expect(200)
-      .then(async() => {
-        const payment_requests = await paymentRequestAPI.getPaymentRequests();
-          expect(payment_requests.length).toBe(initialLength - 1);
+    await request
+      .post("/auth/login")
+      .send(payload)
+      .expect("Content-Type", /json/)
+      .expect(200)
+  });
+
+  it("GET payment requests when the user only has access through the group", async () => {
+    await request
+      .get("/paymentrequests")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {      
+        const body = response.body;      
+        expect(body.length).toBe(1);
       })
       .catch((err) => {
-          expect(err).toBe(null);
+        expect(err).toBe(null);
       });
+  });
+
+  it("POST /paymentrequests/update - Attempt to update payment request in a viewable receipt but is unrelated to the user", async () => {
+    const payment_requests = await paymentRequestAPI.getPaymentRequests();
+    const payment_request_ids = payment_requests.map(p => p.payment_request_id);
+
+    const payload = {
+      description: 'lawn mowing'
+    }
+    
+    await request
+        .post("/paymentrequests/" + payment_request_ids[1] + "/update")
+        .send(payload)
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(401)
+  });
+
+  it("POST /paymentrequests/delete - Attempt to delete payment request in a viewable receipt but is unrelated to the user", async () => {
+    const payment_requests = await paymentRequestAPI.getPaymentRequests();
+    const payment_request_ids = payment_requests.map(p => p.payment_request_id);
+    
+    await request
+        .post("/paymentrequests/" + payment_request_ids[1] + "/delete")
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(401)
+  });
 });
